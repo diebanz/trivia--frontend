e63141d8d31e75ee5e4a95ebd0163948fb9e76e7
Natalia Banz <banznatalia@gmail.com>
2022-01-18 12:12:21 +0100

    userprofile.jsx

--- src/components/UserProfile/CardUserProfile.jsx
+++ src/components/UserProfile/CardUserProfile.jsx
@@ -1,7 +1,47 @@
 import React from "react";
 
+import { FaEdit } from "react-icons/fa";
+
+import classes from "./../../sass/components/UserProfile/CardUserProfile.module.scss";
+
 function CardUserProfile() {
-    return <div>awdawe</div>;
+    return (
+        <div className={classes.profile}>
+            <section className={classes.profile__user}>
+                <div className={classes["profile__user--piccontainer"]}>
+                    <div className={classes["profile__user--pic"]}></div>
+                    <div className={classes["profile__user--year"]}>1988</div>
+                </div>
+                <div className={classes["profile__user--namecontainer"]}>
+                    <div className={classes["profile__user--name"]}>
+                        Crazy CAT
+                    </div>
+                    <div className={classes["profile__user--email"]}>
+                        Crazy CAT@catcat.at
+                    </div>
+                    <div className={classes["profile__user--statcontainer"]}>
+                        <div className={classes["profile__user--recentachiev"]}>
+                            Warmed up!
+                        </div>
+                        <div className={classes["profile__user--currentrank"]}>
+                            Rank 10
+                        </div>
+                    </div>
+                </div>
+                <div className={classes["profile__user--iconcontainer"]}>
+                    <FaEdit className={classes["profile__user--icon"]} />
+                </div>
+            </section>
+            <section className={classes.profile__stats}>
+                <div className={classes["profile__stats--titles"]}>
+                    <h4 className="heading heading__4">Ranking</h4>
+                    <h4 className="heading heading__4">Statistics</h4>
+                    <h4 className="heading heading__4">Achievements</h4>
+                </div>
+                <div className={classes["profile__stats--data"]}></div>
+            </section>
+        </div>
+    );
 }
 
 export default CardUserProfile;
--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -4,14 +4,14 @@ import LogSquare from "../common/LogSquare";
 import CardGameDashboard from "../components/UserProfile/CardGameDashboard";
 import CardUserProfile from "../components/UserProfile/CardUserProfile";
 
-import "./../sass/pages/UserProfile.module.scss";
+import classes from "./../sass/pages/UserProfile.module.scss";
 
 function UserProfile() {
     return (
-        <div>
+        <div className={classes.user}>
+            <LogSquare />
             <CardUserProfile />
             <CardGameDashboard />
-            <LogSquare />
         </div>
     );
 }
