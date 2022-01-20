82472017c583c861876e0214b3abdd6effd0c2ba
Tobias Pfaffenzeller <tobler@gmx.com>
2022-01-20 15:59:35 +0100

    flags

--- src/components/UserProfile/Card/CardUserProfileUser.jsx
+++ src/components/UserProfile/Card/CardUserProfileUser.jsx
@@ -3,7 +3,10 @@ import axios from "axios";
 
 import { FaEdit } from "react-icons/fa";
 
+import {flags} from "../../../common/flags.js"
 import classes from "./../../../sass/components/UserProfile/Card/CardUser.module.scss";
+import { url } from "inspector";
+
 
 function CardUserProfileUser() {
     const [data, setData] = useState();
@@ -15,36 +18,61 @@ function CardUserProfileUser() {
             console.log(response.data);
             if (response.data.message === "success")
                 setData(response.data.payload);
+            
         })();
     }, []);
+
+
+
+    function flagChangeHandler () {
+
+    }
+
+
     return (
-        <section className={classes.profile__user}>
-            <div className={classes["profile__user--piccontainer"]}>
-                <div className={classes["profile__user--pic"]}></div>
-                <div className={classes["profile__user--year"]}>
-                    <p>{(data && data.dob) || 'set DoB'}</p>
+       <section className={classes.profile__user}>
+          <div className={classes["profile__user--piccontainer"]}>
+             <div className={classes["profile__user--pic"]}>
+                <div
+                   className={classes["profile__user--flag"]}
+                   style={{
+                      backgroundImage: `${
+                         data && data.nat ? data.nat : "https://flagcdn/64x48/un.png"
+                      }`,
+                   }}
+                   onClick={flagChangeHandler}
+                ></div>
+             </div>
+             <div className={classes["profile__user--year"]}>
+                <p>{(data && data.dob) || "set DoB"}</p>
+             </div>
+          </div>
+          <div className={classes["profile__user--namecontainer"]}>
+             <div className={classes["profile__user--name"]}>
+                <p>{data && data.username}</p>
+             </div>
+             <div className={classes["profile__user--email"]}>
+                <p>{(data && data.email) || "enter your email here"}</p>
+             </div>
+             <div className={classes["profile__user--statcontainer"]}>
+                <div className={classes["profile__user--recentachiev"]}>
+                   Warmed up!
                 </div>
-            </div>
-            <div className={classes["profile__user--namecontainer"]}>
-                <div className={classes["profile__user--name"]}>
-                    <p>{data && data.username}</p>
+                <div className={classes["profile__user--currentrank"]}>
+                   {/* {data && data.} */}
                 </div>
-                <div className={classes["profile__user--email"]}>
-                    <p>{(data && data.email) || 'enter your email here'}</p>
-                </div>
-                <div className={classes["profile__user--statcontainer"]}>
-                    <div className={classes["profile__user--recentachiev"]}>
-                        Warmed up!
-                    </div>
-                    <div className={classes["profile__user--currentrank"]}>
-                        {/* {data && data.} */}
-                    </div>
-                </div>
-            </div>
-            <div className={classes["profile__user--iconcontainer"]}>
-                <FaEdit className={classes["profile__user--icon"]} />
-            </div>
-        </section>
+             </div>
+          </div>
+          <div className={classes["profile__user--iconcontainer"]}>
+             <FaEdit className={classes["profile__user--icon"]} />
+          </div>
+
+          <select ref={selectRef}>
+              {flags.map((flag, i)=> {
+                  return <option value={flag.keys[i]} style={{backgroundImage: url(flag[i])}}></option>
+              })}
+          </select>
+       </section>
     );
 }
 
