ce7003d8e99f584f6e833222a357b69ee439afa0
FloHGer <mail@florianhoehle.de>
2022-01-21 11:38:42 +0100

    Work on achievements tab

--- src/components/UserProfile/Card/Data/Achievs/AchievCard.jsx
+++ src/components/UserProfile/Card/Data/Achievs/AchievCard.jsx
@@ -13,9 +13,10 @@ function AchievCard({ data, title, text, values }) {
     // );
     // const year = data.unlocked[data.unlocked.length - 1].getFullYear();
 
+
     return (
         <div className={classes.card}>
-            <div
+            {/* <div
                 className={classes.card__unlocked}
                 style={{
                     width: `${
@@ -25,35 +26,35 @@ function AchievCard({ data, title, text, values }) {
                                 values[data.unlocked.length - 1])
                     }%`,
                 }}
-            ></div>
+            ></div> */}
             <div className={classes.card__container}>
                 <div className={classes.card__square}></div>
 
-                <div className={classes.card__date}></div>
-            </div>
-            <div className={classes.card__data}>
-                {/* <div className={classes.card__date}>
-                    {(data.unlocked.length && (
+                <div className={classes.card__date}>
+                    <p>{(data.unlocked.length && (
                         <p>{data.unlocked[data.unlocked.length - 1]}</p>
                     )) ||
-                        ""}
-                </div> */}
+                        ""}</p>
+                </div>
+            </div>
+            <div className={classes.card__data}>
                 <div className={classes.card__text}>
                     <h2 className={classes["card__data--title"]}>{title}</h2>
                     <h3 className={classes["card__data--condition"]}>
                         {text[0]}
-                        {values[data.unlocked.length]}
+                        {data.unlocked.length === values.length ? values[values.length -1] : values[data.unlocked.length]}
                         {text[1]}
                     </h3>
 
-                    <h4 className={classes["card__data--todo"]}>
-                        {" "}
-                        <div
-                            className={classes.card__progression}
-                            style={{ width: `${100 - data.next}%` }}
-                        ></div>
-                        {data.next} / 10
-                    </h4>
+                    {data.unlocked.length < values.length &&
+                        <h4 className={classes["card__data--todo"]}>
+                            <div
+                                className={classes.card__progression}
+                                style={{width: `${(values[data.unlocked.length] - data.next) * 100 / values[data.unlocked.length]}%`,}}
+                            ></div>
+                            {!data.unlocked.length && values.length === 6 ? 0 : values[data.unlocked.length] - data.next} / {values[data.unlocked.length]}
+                        </h4>
+                    }
                 </div>
             </div>
         </div>
@@ -61,3 +62,7 @@ function AchievCard({ data, title, text, values }) {
 }
 
 export default AchievCard;
+
+
+// 100% = values[data.unlocked.length]
+// x% = (values[data.unlocked.length] - data.next) * 100 / values[data.unlocked.length]
